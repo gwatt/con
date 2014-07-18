b1407810635b1b04e4fe1cc7fb6ad3f7feb25afc
Graham Watt <gmw@doublemap.com>
2014-07-18 11:44:37 -0400

    Filled out baudval function.

--- con.c
+++ con.c
@@ -6,6 +6,8 @@
 #include <termios.h>
 #include <unistd.h>
 
+
+/* Maps baudrates to the B[0-9]+ values defined in termios.h */
 int baudval(int baudrate);
 
 int con(int fd);
@@ -25,7 +27,25 @@ int con(int fd) {
 }
 
 int baudval(int baudrate) {
+#define B(x) {B##x, x}
+	static const struct {
+		int val;
+		int rate;
+	} bauds = {
+		B(0), B(50), B(75), B(110), B(134), B(200), B(300), B(600), B(1200),
+		B(1800), B(2400), B(4800), B(9600), B(19200), B(38400), B(57600),
+		B(115200), B(230400), B(460800), B(500000), B(576000), B(921600),
+		B(1000000), B(1152000), B(1500000), B(2000000), B(2500000), B(3000000),
+		B(3500000), B(4000000), {-1, -1}
+	};
+	int i;
 	int baud = -1;
+	for (i = 0; bauds[i].val != -1; i++) {
+		if (bauds[i].rate == baudrate) {
+			baud = bauds[i].val;
+			break;
+		}
+	}
 
 	return baud;
 }
